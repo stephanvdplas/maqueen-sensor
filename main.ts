edb8cc072afa5a03ffd7b981e3c5fb9da6ac8389
Stephan van der Plas <stephan@vdplas.net>
2023-01-27 13:59:40 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ let afstand = 0
 basic.forever(function () {
     afstand = DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2)
     if (afstand > 15) {
+        basic.showNumber(afstand)
         music.stopAllSounds()
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 101)
     } else {
