2234f0149bae7638dc7b047306331d4ac35cb75e
Stephan van der Plas <stephan@vdplas.net>
2023-01-27 17:52:36 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 let afstand = 0
+music.setVolume(60)
 basic.forever(function () {
     afstand = DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2)
     if (afstand > 15) {
         basic.showNumber(afstand)
         music.stopAllSounds()
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 101)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 255)
     } else {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        music.ringTone(100 * afstand)
+        music.playTone(75 * afstand, music.beat(BeatFraction.Quarter))
         basic.pause(500)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 93)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 150)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 25)
     }
 })
